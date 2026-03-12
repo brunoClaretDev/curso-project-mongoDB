ef9ea371b532b67b8f31c8617bbaa3c0eae3afa5
brunoClaretDev <brunoclaret.dev@gmail.com>
2026-03-11 22:03:35 -0300

    Projected created

---
+++ src/test/java/com/brunoClaretDev/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoClaretDev.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
